bda190f8739f12b47cdac64f6e45b413cb695247
Lotustrailtheprestigefig <102186419+Lotustrailtheprestigefig@users.noreply.github.com>
2023-06-09 17:37:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -104,58 +104,230 @@ conflicted = "🏳"
 up_to_date = " "
 untracked = " "
 ahead = "⇡\${count}"
-diverged = "⇕\${count}"
+diverged = "⇕⇡\${ahead_count}⇣\${behind_count}"
 behind = "⇣\${count}"
-stashed = "💾 "
-modified = "\${count}"
-staged = "✚\${count}"
-renamed = "➜\${count}"
-deleted = "✖\${count}"
+stashed = " "
+modified = " "
+staged = '[++\(\$count\)](green)'
+renamed = "襁 "
+deleted = " "
 
-[cmd_duration]
-format = "took [\$duration](bold yellow) "
+[kubernetes]
+format = 'via [ﴱ \$context(\$namespace)](bold purple) '
+disabled = false
 
-[custom.foo]
-description = "An example custom prompt segment"
-format = "says [\"hello! \"](bold green)"
+[username]
+disabled = true
 
-[custom.foo.env_var]
-format = "(\$value)"
-variable = "MY_VAR"
+[vagrant]
+disabled = true
+
+[docker_context]
+disabled = true
+
+[helm]
+disabled = true
+
+[python]
+disabled = true
+
+[nodejs]
+disabled = true
+
+[ruby]
+disabled = true
+
+[terraform]
+disabled = true
+
+[localip]
+ssh_only = false
+format = '@[\$localipv4](bold red) '
+disabled = false
+
+[memory_usage]
+disabled = false
+threshold = -1
+symbol = ' '
+style = 'bold dimmed green'
+
+[time]
+disabled = false
+format = '🕙[\[ \$time \]](\$style) '
+time_format = '%T'
+utc_time_offset = '+2'
 EOF
 echo "starship.toml configuration file created."
 
-# Add additional configuration to /etc/zsh/zshrc
+# Append additional configuration to /etc/zsh/zshrc
 echo "Adding additional configuration to /etc/zsh/zshrc..."
-cat <<EOF | sudo tee -a /etc/zsh/zshrc
-# Load custom configurations
-for config_file ($HOME/.zsh/*.zsh); do
-    source "\$config_file"
-done
+cat <<'EOF' >> /etc/zsh/zshrc
+# Useful support for interacting with Terminal.app or other terminal programs
+[ -r "/etc/zshrc_$TERM_PROGRAM" ] && . "/etc/zshrc_$TERM_PROGRAM"
+[[ -f ~/.zsh/aliases.zsh ]] && source ~/.zsh/aliases.zsh
+[[ -f ~/.zsh/functions.zsh ]] && source ~/.zsh/functions.zsh
+[[ -f ~/.zsh/starship.zsh ]] && source ~/.zsh/starship.zsh
+[[ -f ~/.zsh/nvm.zsh ]] && source ~/.zsh/nvm.zsh
+[[ -f ~/.zsh/wsl2fix.zsh ]] && source ~/.zsh/wsl2fix.zsh
+[[ -f ~/.zsh/goto.zsh ]] && source ~/.zsh/goto.zsh
+# Load Direnv
+#eval "$(direnv hook zsh)"
+eval "$(starship init zsh)"
 EOF
 echo "Additional configuration added to /etc/zsh/zshrc."
 
-# Create additional files in ~/.zsh directory
-echo "Creating additional files in ~/.zsh directory..."
+# Create .zsh directory in the home folder
+echo "Creating .zsh directory..."
 mkdir -p ~/.zsh
-touch ~/.zsh/aliases.zsh
-touch ~/.zsh/functions.zsh
-touch ~/.zsh/goto.zsh
-touch ~/.zsh/nvm.zsh
-touch ~/.zsh/starship.zsh
-echo "Additional files created in ~/.zsh directory."
+echo ".zsh directory created."
 
-# Source the ~/.zshrc file to apply changes
-echo "Sourcing ~/.zshrc file..."
-source ~/.zshrc
-echo "Changes applied."
+# Create aliases.zsh file
+echo "Creating aliases.zsh file..."
+cat <<'EOF' > ~/.zsh/aliases.zsh
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+alias dt="datree"
 
-# Ask if the user wants to reboot the system
-read -p "Do you want to reboot the system? [y/n]: " reboot_system
+# mac OS shortcuts
+alias code="open -a 'Visual Studio Code'"
 
-if [[ $reboot_system == "y" || $reboot_system == "Y" ]]; then
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias g="goto"
+alias grep='grep --color'
+
+alias cbp="code /home/xcad/obsidianvault/boilerplates"
+alias cpr="code /home/xcad/obsidianvault/projects"
+
+# Scripts
+alias repos="op run --env-file="/Users/xcad/.env" -- python3 /Users/xcad/Projects/christianlempa/scripts/repos"
+EOF
+echo "aliases.zsh file created."
+
+# Create functions.zsh file
+echo "Creating functions.zsh file..."
+cat <<'EOF' > ~/.zsh/functions.zsh
+# Colormap
+function colormap() {
+  for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
+}
+EOF
+echo "functions.zsh file created."
+
+# Create goto.zsh file
+echo "Creating goto.zsh file..."
+cat <<'EOF' > ~/.zsh/goto.zsh
+autoload -Uz compinit
+compinit
+
+source $(brew --prefix)/etc/bash_completion.d/goto.sh
+EOF
+echo "goto.zsh file created."
+
+# Create nvm.zsh file
+echo "Creating nvm.zsh file..."
+cat <<'EOF' > ~/.zsh/nvm.zsh
+# NVM lazy load
+if [ -s "$HOME/.nvm/nvm.sh" ]; then
+  [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+  alias nvm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && nvm'
+  alias node='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && node'
+  alias npm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && npm'
+fi
+EOF
+echo "nvm.zsh file created."
+
+# Create starship.zsh file
+echo "Creating starship.zsh file..."
+cat <<'EOF' > ~/.zsh/starship.zsh
+# find out which distribution we are running on
+LFILE="/etc/*-release"
+MFILE="/System/Library/CoreServices/SystemVersion.plist"
+if [[ -f $LFILE ]]; then
+  _distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
+elif [[ -f $MFILE ]]; then
+  _distro="macos"
+
+  # on mac os use the systemprofiler to determine the current model
+  _device=$(system_profiler SPHardwareDataType | awk '/Model Name/ {print $3,$4,$5,$6,$7}')
+
+  case $_device in
+    *MacBook*)     DEVICE="";;
+    *mini*)        DEVICE="󰇄";;
+    *)             DEVICE="";;
+  esac
+fi
+
+# set an icon based on the distro
+# make sure your font is compatible with https://github.com/lukas-w/font-logos
+case $_distro in
+    *kali*)                  ICON="ﴣ";;
+    *arch*)                  ICON="";;
+    *debian*)                ICON="";;
+    *raspbian*)              ICON="";;
+    *ubuntu*)                ICON="";;
+    *elementary*)            ICON="";;
+    *fedora*)                ICON="";;
+    *coreos*)                ICON="";;
+    *gentoo*)                ICON="";;
+    *mageia*)                ICON="";;
+    *centos*)                ICON="";;
+    *opensuse*|*tumbleweed*) ICON="";;
+    *sabayon*)               ICON="";;
+    *slackware*)             ICON="";;
+    *linuxmint*)             ICON="";;
+    *alpine*)                ICON="";;
+    *aosc*)                  ICON="";;
+    *nixos*)                 ICON="";;
+    *devuan*)                ICON="";;
+    *manjaro*)               ICON="";;
+    *gentoo*)                ICON="";;
+    *void*)                  ICON="";;
+    *freebsd*)               ICON="";;
+    *macos*)                 ICON="";;
+    *ios*)                   ICON="";;
+    *fedora*)                ICON="";;
+    *)                       ICON="";;
+esac
+
+export STARSHIP_DEVICE=$DEVICE
+export STARSHIP_DISTRO=$ICON
+EOF
+echo "starship.zsh file created."
+
+# Create wsl2fix.zsh file
+echo "Creating wsl2fix.zsh file..."
+cat <<'EOF' > ~/.zsh/wsl2fix.zsh
+if [[ "$(uname -r)" == *Microsoft ]]; then
+    bindkey -m emacs '^?' backward-delete-char
+    bindkey -m viins '^?' backward-delete-char
+    bindkey -m vi-command '^?' backward-delete-char
+    bindkey -m isearch '^?' backward-delete-char
+
+    alias code='code.cmd'
+fi
+EOF
+echo "wsl2fix.zsh file created."
+
+# Source the ~/.zshrc file
+echo "Sourcing the ~/.zshrc file..."
+source /etc/zsh/zshrc
+echo "Installation complete."
+sleep 2
+
+# Ask if the user wants to reboot
+read -p "Installation complete. Do you want to reboot the system now? [y/n]: " reboot_choice
+
+if [[ $reboot_choice == "y" || $reboot_choice == "Y" ]]; then
     echo "Rebooting the system..."
-    reboot
+    sudo reboot
 else
-    echo "Script execution completed."
+    echo "Installation complete. Please reboot the system to apply the changes."
 fi
